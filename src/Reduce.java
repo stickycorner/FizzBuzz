962f0a463b8b037a1e1dcfcacb44c873ac496eb2
stickycorner <jennazhang.jz@gmail.com>
2024-09-09 14:27:59 -0400

    created reduce function

---
+++ src/Reduce.java
@@ -0,0 +1,22 @@
+public class Reduce {
+    public static void main(String[] args) {
+        Reduce.Reduction(100);
+    }
+
+    private static int Reduction(int n) {
+        int count = 0;
+        if (n == 0) {
+            return 0;
+        }
+        else if (n % 2 == 1) {
+            n = n - 1;
+            count = 1 + Reduction(n);
+        }
+        else { // n % 2 == 0
+            n = n / 2;
+            count = 1 + Reduction(n);
+        }
+        System.out.println(count);
+        return count;
+    }
+}
